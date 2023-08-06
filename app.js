7c9128e928c1e6d237b50d9796bfd2ad4e76c74d
shipanchen <shipanchen71@gmail.com>
2023-08-06 01:42:26 -0400

    a5 backend 4

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import "dotenv/config";
 import express from 'express';
 import session from "express-session";
 import cors from 'cors'
-import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
@@ -32,10 +31,8 @@ app.use(
 );
 
 app.use(express.json());
-// app.get('/hello', (req, res) => {res.send('Life is good!')})
-// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 TuitsController(app);
-HelloController(app)
+// HelloController(app)
 UserController(app)
 AuthController(app)
 // app.listen(4000)
