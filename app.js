ba2c188bc19ab3318b9b89ed80cd1e0c9c63f02b
shipanchen <shipanchen71@gmail.com>
2023-08-06 21:20:38 -0400

    a5 push 0806

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// const express = require('express')
 import "dotenv/config";
 import express from 'express';
 import session from "express-session";
@@ -6,11 +5,11 @@ import cors from 'cors'
 import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
+import HelloController from "./controllers/hello-controller.js";
 const app = express()
 app.use(
     cors({
         credentials: true,
-        // origin: "http://localhost:3000",
         origin: process.env.FRONTEND_URL
     })
 );
@@ -26,14 +25,14 @@ const sessionOptions = {
             secure: true,
         };
 }
-app.use(
-    session(sessionOptions)
+    app.use(
+        session(sessionOptions)
 );
 
 app.use(express.json());
 AuthController(app)
 TuitsController(app);
-//HelloController(app)
+HelloController(app)
 UserController(app)
 // app.listen(4000)
 app.listen(process.env.PORT || 4000);
