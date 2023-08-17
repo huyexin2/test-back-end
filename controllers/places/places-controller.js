56348eb233c66bafa8e74988877bb811aceff327
huyexin1 <yexin.hu@mail.utoronto.ca>
2023-08-16 23:30:00 -0400

    modifaction

--- controllers/places/places-controller.js
+++ controllers/places/places-controller.js
@@ -4,15 +4,25 @@ import axios from "axios";
 
 const createPlaces = async (req, res) => {
     const newPlace = req.body;
+    console.log(newPlace)
     places.push(newPlace);
     res.json(newPlace);
 }
 
 const findPlaces  = (req, res) => { res.json(places);}
 
+const deletePlaces = (req, res) => {
+    const placeToDelete = req.params.status;
+    places = places.filter((t) =>
+        t.status !== placeToDelete);
+    res.sendStatus(200);
+
+}
+
 
 export default (app) => {
     app.post('/api/places', createPlaces);
     app.get('/api/places', findPlaces);
+    app.delete('/api/places/:status', deletePlaces);
 }
 
